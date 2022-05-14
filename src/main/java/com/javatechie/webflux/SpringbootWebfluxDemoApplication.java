b93ad1dc7282a0ba3354b01e7b322b8f306733cf
CarlChen88 <545663272@qq.com>
2022-05-14 21:58:53 +0800

    test

--- src/main/java/com/javatechie/webflux/SpringbootWebfluxDemoApplication.java
+++ src/main/java/com/javatechie/webflux/SpringbootWebfluxDemoApplication.java
@@ -1,9 +1,11 @@
 package com.javatechie.webflux;
 
+import com.javatechie.webflux.config.EnableConfigurationImport;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableConfigurationImport
 public class SpringbootWebfluxDemoApplication {
 
 	public static void main(String[] args) {
