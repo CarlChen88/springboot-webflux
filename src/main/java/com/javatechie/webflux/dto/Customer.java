4e485ee0d4b9ac6f167242270cb5ecf3e7b34eb6
CarlChen88 <545663272@qq.com>
2022-05-14 20:11:20 +0800

    test

---
+++ src/main/java/com/javatechie/webflux/dto/Customer.java
@@ -0,0 +1,35 @@
+package com.javatechie.webflux.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public class Customer {
+
+    private int id;
+    private String name;
+
+    public Customer() {
+    }
+
+    public Customer(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/javatechie/webflux/router/RouterConfig.java
@@ -0,0 +1,29 @@
+package com.javatechie.webflux.router;
+
+import com.javatechie.webflux.handler.CustomerHandler;
+import com.javatechie.webflux.handler.CustomerStreamHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.*;
+
+@Configuration
+public class RouterConfig {
+
+    @Autowired
+    private CustomerHandler handler;
+
+    @Autowired
+    private CustomerStreamHandler streamHandler;
+
+    @Bean
+    public RouterFunction<ServerResponse> routerFunction(){
+        /*return RouterFunctions.route()
+                .GET("/router/customers",handler::loadCustomers)
+                .GET("/router/customers/stream",streamHandler::getCustomers)
+                .GET("/router/customer/{input}",handler::findCustomer)
+                .POST("/router/customer/save",handler::saveCustomer)
+                .build();*/
+        return RouterFunctions.route(RequestPredicates.GET("/router/customer/{input}"),handler::findCustomer);
+    }
+}
