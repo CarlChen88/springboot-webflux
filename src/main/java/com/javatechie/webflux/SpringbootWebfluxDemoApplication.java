dbfd8ec0847e06cb54e1d795ca2605e28ff83888
CarlChen88 <545663272@qq.com>
2022-05-21 21:01:02 +0800

    test

--- src/main/java/com/javatechie/webflux/SpringbootWebfluxDemoApplication.java
+++ src/main/java/com/javatechie/webflux/SpringbootWebfluxDemoApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootWebfluxDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringbootWebfluxDemoApplication.class, args);
 	}
 
